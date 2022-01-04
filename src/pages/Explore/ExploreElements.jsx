1df15af7d06cc5e0ef7154b6df2c4b1de246d671
AlvaJufinto <yeahbruh199@gmail.com>
2022-01-04 22:03:57 +0700

    Developing explore page

--- src/pages/Explore/ExploreElements.jsx
+++ src/pages/Explore/ExploreElements.jsx
@@ -4,11 +4,13 @@ import styled from "styled-components";
 import { GlobalColors, GlobalMeasurement } from "../../globals";
 
 export const ExploreSection = styled.div`
+    min-height: 100vh;
     background: ${GlobalColors.black};
     margin: ${GlobalMeasurement.navbarHeight}px 0 0 0;
     padding: 20px 20px 0px 20px;
     display: flex;
     justify-content: center;
+    /* align-items: center; */
     
     @media (max-width: 1000px) {
         padding: 20px 20px 60px 20px;
@@ -16,8 +18,28 @@ export const ExploreSection = styled.div`
 `
 
 export const ExploreContainer = styled.div`
-    min-height: 100vh;
+    height: 10px;
     width: ${GlobalMeasurement.containerWidth}px;
     display: flex;
-    position: relative;
+    flex-wrap: wrap;
+    gap: 10px;
+    background: orange;
+`
+
+export const ExploreCard = styled.div`
+    background: red;
+    height: 300px;
+    width: 300px;
+    flex-grow: 1;
+`
+export const ExploreCardOverlay = styled.div`
+
+`
+
+export const ExploreCardContent = styled.div`
+
+`
+
+export const ExploreCardContentImg = styled.div`
+
 `
--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,3 +1,8 @@
+import {
+    ThumbUp, 
+    ThumbUpAltOutlined 
+} from '@mui/icons-material';
+
 import Navbar from '../../components/Nav';
 import Sidebar from "../../components/Sidebar";
 import Feed from "../../components/Feed";
@@ -5,19 +10,45 @@ import Rightbar from "../../components/Rightbar";
 
 import {
     ExploreSection,
-    ExploreContainer
+    ExploreContainer,
+    ExploreCard,
+    ExploreCardOverlay,
+    ExploreCardContent,
+    ExploreCardContentImg,
 } from "./ExploreElements";
 
 const Explore = () => {
+    let items = ['1','2','3','4'];
+
     return ( 
         <>
             <Navbar />
             <ExploreSection>
                 <ExploreContainer>
+                    {items.map(item => {
+                        return (
+                            <ExploreCard>
+                            <ExploreCardOverlay>
+                                <ThumbUp />
+                                {item}
+                                <ThumbUpAltOutlined />
+                                20
+                            </ExploreCardOverlay>
+                            <ExploreCardContent>
+                                <ExploreCardContentImg />
+                            </ExploreCardContent>
+                            </ExploreCard>
+                        )
+                    })}
                     <ExploreCard>
-                        <ExploreCardOverlay></ExploreCardOverlay>
+                        <ExploreCardOverlay>
+                            <ThumbUp />
+                            10
+                            <ThumbUpAltOutlined />
+                            20
+                        </ExploreCardOverlay>
                         <ExploreCardContent>
-                            
+                            <ExploreCardContentImg />
                         </ExploreCardContent>
                     </ExploreCard>
                 </ExploreContainer>
