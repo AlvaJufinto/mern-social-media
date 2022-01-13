41fc06420d8ad929df173cdba3d4d9e3d11f61ba
AlvaJufinto <yeahbruh199@gmail.com>
2022-01-13 21:24:55 +0700

    Login authroute is done

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcrypt');
 
 // Register 
 router.post("/register", async (req, res) => {
-    
     try {
         // Generate new password
         const salt = await bcrypt.genSalt(10);
@@ -25,7 +24,21 @@ router.post("/register", async (req, res) => {
         res.status(500).json(err);
         console.log(err);
     }
-    
+});
+
+// Login
+router.post('/login', async (req, res) =>{
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).json("User not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(404).json("Wrong password");
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
