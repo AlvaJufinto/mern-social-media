4439cf94efe8120560f6bfb4556140a92fa6e1ed
AlvaJufinto <yeahbruh199@gmail.com>
2022-01-13 21:16:46 +0700

    Finishing up Register Authroute

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const mongoose = require('mongoose');
+// const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.send("Hello world");
-});
+// Route
+const authRoute = require('./routes/auth');
+
+// Optional vars
+const morgan = require('morgan');
+const port = 8000;
+require('dotenv').config();
+
+// middleware
+app.use(morgan('common'));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGO_CONNECT,
+  { 
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    autoIndex: true, //make this also true  
+  },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
     console.log(`App is running at port ${port}`);
 });
-
-
 
--- models/User.js
+++ models/User.js
@@ -1,64 +1,72 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new.mongoose.Schema({
-    username: {
-        type: String, 
-        require: true,
-        min: 3,
-        max: 20,
-        unique: true,
+const UserSchema = new mongoose.Schema({
+        username: {
+            type: String, 
+            required: [true, 'Username is required'],
+            minLength: 3,
+            maxLength: 20,
+            unique: true,
+        },
+        fullname:{
+            type: String,
+            required: [true, 'Fullname is required'],
+            minLength: 6,
+            maxLength: 50,
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            maxLength: 50,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Username, fullname, or email is required'],
+            minLength: [6, "Password must be at least 6 characters"],
+        },
+        profilePicture: {
+            type: String,
+            default: "",
+        },
+        coverPicture: {
+            type: String,
+            default: "",
+        },
+        description: {
+            type: String,
+            maxLength: 50,
+        },
+        followers: {
+            type: Array,
+            default: [],
+        },
+        followings: {
+            type: Array,
+            default: [],
+        },
+        city: {
+            type: String,
+            maxLength: 20,
+        }, 
+        from : {
+            type: String,
+            maxLength: 20,      
+        },
+        work : {
+            type: String,
+            maxLength: 20
+        },
+        relationship: {
+            type: String,
+            maxLength: 10,
+        },
+        website: {
+            type: String,
+            maxLength: 10,
+        },  
     },
-    fullname:{
-        type: String,
-        require: true,
-        max: 50,
-        unique: true,
-    },
-    email: {
-        type: String,
-        require: true,
-        max: 50,
-    },
-    profilePicture: {
-        type: String,
-        default: "",
-    },
-    coverPicture: {
-        type: String,
-        default: "",
-    },
-    description: {
-        type: String,
-        max: 50,
-    },
-    followers: {
-        type: Array,
-        default: [],
-    },
-    followings: {
-        type: Array,
-        default: [],
-    },
-    city: {
-        type: String,
-        max: 20,
-    }, 
-    from : {
-        type: String,
-        max: 20,      
-    },
-    work : {
-        type: String,
-        max: 20
-    },
-    relationship: {
-        type: String,
-        max: 10,
-    },
-    website: {
-        type: String,
-        max: 10,
-    }
-});
+    { timestamps: true }, 
+);
 
-module.exports = mongoose.model("Post", PostSchema);
+module.exports = mongoose.model("User", UserSchema);
