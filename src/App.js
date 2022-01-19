a633fd61ab7f12ab5d72b756037c7d731a4a1073
AlvaJufinto <yeahbruh199@gmail.com>
2022-01-19 22:09:30 +0700

    Configurating redirecting system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 import { GlobalStyles } from "./globals";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Redirect
+  Navigate
 } from "react-router-dom";
 
 import SignUp from "./pages/SignUp";
@@ -14,16 +16,18 @@ import Explore from "./pages/Explore";
 import PostPage from "./pages/PostPage";
 
 const App = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <GlobalStyles />
       <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route exact path="/explore" element={<Explore />}/>
-        <Route exact path="/post/:id" element={<PostPage />}/>
-        <Route exact path="/profile/:id" element={<Profile />}/>
-        <Route exact path="/sign-in" element={<SignIn />}/>
-        <Route exact path="/sign-up" element={<SignUp />}/>
+        <Route exact path="/" element={user ? <Home /> : <Navigate to="/sign-in" /> }/>
+        <Route exact path="/explore" element={user ? <Explore /> : <Navigate to="/sign-in" /> }/>
+        <Route exact path="/post/:id" element={user ? <PostPage /> : <Navigate to="/sign-in" /> }/>
+        <Route exact path="/profile/:id" element={user ? <Profile /> : <Navigate to="/sign-in" /> }/>
+        <Route exact path="/sign-in" element={user ? <Navigate to="/" /> :  <SignIn />}/>
+        <Route exact path="/sign-up" element={user ? <Navigate to="/" /> : <SignUp />}/>
       </Routes>
     </Router>
   );
