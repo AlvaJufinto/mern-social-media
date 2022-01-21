953f5f482f4fec87156318fb3196b0599700a1bd
AlvaJufinto <yeahbruh199@gmail.com>
2022-01-21 15:48:42 +0700

    Developing login system on server side

--- index.js
+++ index.js
@@ -8,12 +8,24 @@ const authRoute = require('./routes/auth');
 
 // Optional vars
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require("cors");
 const port = 8000;
 require('dotenv').config();
+
+// CORS Config
+const corsOptions = {
+    origin: '*', 
+    credentials: true,            
+    // access-control-allow-credentials: true,
+    // optionSuccessStatus: 200,
+}
 
 // middleware
 app.use(morgan('common'));
 app.use(express.json());
+app.use(helmet());
+app.use(cors(corsOptions));
 
 mongoose.connect(
   process.env.MONGO_CONNECT,
@@ -28,6 +40,7 @@ mongoose.connect(
 );
 
 app.use("/api/auth", authRoute);
+// app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
     console.log(`App is running at port ${port}`);
