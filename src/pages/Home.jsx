2149c3b1268cac483ea32d09859d9a9c2d406843
unknown <yeahbruh199@gmail.com>
2022-05-05 10:57:34 +0700

    ADD: home page and minor changes to some sign up page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import {
+  Link
+} from "react-router-dom";
+
+import './../styles/pages-css/auth.css';
+
+const Home = () => {
+
+  return (
+    <div className="HomeContainer">
+      
+    </div>
+  );
+}
+
+export default Home;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -13,31 +13,31 @@ const SignUp = () => {
               <h1 className="AuthWrapper__title">Synchronize</h1>
               <p className="AuthWrapper__description">Let’s get started</p>
               <div className="Auth__input-group">
-                  <p className="Auth__input-group__Name" >Username</p>
+                  <p className="Auth__input-group__Name">Username</p>
                   <div className="Auth__input-group__wrapper input-wrapper-global">
                     <i className="fa-solid fa-circle-user Auth__input-icon"></i>
-                    <input type="text" placeholder="ex. MikeOxlong69"  className="Auth__input-text input-text-global"></input>
+                    <input type="text" placeholder="ex. MikeOxlong69" className="Auth__input-text input-text-global"></input>
                   </div>
               </div>
               <div className="Auth__input-group">
-                  <p className="Auth__input-group__Name" >Fullname</p>
+                  <p className="Auth__input-group__Name">Fullname</p>
                   <div className="Auth__input-group__wrapper input-wrapper-global">
                     <i className="fa-solid fa-circle-user Auth__input-icon"></i>
-                    <input type="text" placeholder="ex. MikeOxlong69"  className="Auth__input-text input-text-global"></input>
+                    <input type="text" placeholder="ex. My Cocks Lang" className="Auth__input-text input-text-global"></input>
                   </div>
               </div>
               <div className="Auth__input-group">
                   <p className="Auth__input-group__Name" >Email</p>
                   <div className="Auth__input-group__wrapper input-wrapper-global">
                     <i className="fa-solid fa-envelope Auth__input-icon"></i>
-                    <input type="text" placeholder="ex. MikeOxlong69"  className="Auth__input-text input-text-global"></input>
+                    <input type="email" placeholder="ex. MikeOxlong69" className="Auth__input-text input-text-global"></input>
                   </div>
               </div>
               <div className="Auth__input-group">
                   <p className="Auth__input-group__Name" >Password</p>
                   <div className="Auth__input-group__wrapper input-wrapper-global">
                     <i className="fa-solid fa-lock Auth__input-icon"></i>
-                    <input type="password" placeholder="some secret"  className="Auth__input-text input-text-global"></input>
+                    <input type="password" placeholder="some secret" className="Auth__input-text input-text-global"></input>
                   </div>
               </div>            
               <button className="Auth__submit-button button-global">Sign up</button>
