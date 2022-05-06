84081d10154cae38e53d20fd201a3602e3c67dce
unknown <yeahbruh199@gmail.com>
2022-05-06 15:04:56 +0700

    DONE: Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,13 +3,17 @@ import {
   Link
 } from "react-router-dom";
 
-import { ReactComponent as SyncLogo } from "./../assets/svg/sync-logo.svg";
+import { ReactComponent as LikeOutLine } from "./../assets/svg/like_outline.svg";
+import { ReactComponent as LikeBg } from "./../assets/svg/like_bg.svg";
+
 import noAvatar from "./../assets/img/noAvatar.png";
 import noBanner from "./../assets/img/noBanner.jpg";
 
 import './../styles/components-css/post.css';
 
-const Post = () => {
+const Post = () => { 
+  const [likes, setLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
 
   return (
       <div className="PostWrapper container-border-global ">
@@ -18,21 +22,24 @@ const Post = () => {
                 <img src={noAvatar} alt="bruh" className="Post__Top__profile-img" />
                 <p className="Post__Top__username">Alfie Solomons</p>
             </Link>
+
+            <p className="Post__Top__period" >3 days ago</p>
         </div>
         <div className="Post__Middle">
-            <div className="Post__Middle">
-                <p className="Post__Middle__Description">Some cool photo from Ancestral Plane</p>
-                <img src={noBanner} alt="burh" className="Post__Middle__img" />
-            </div>
+            <p className="Post__Middle__Description">Some cool photo from Ancestral Plane</p>
+            <img src={noBanner} alt="burh" className="Post__Middle__img" />
         </div>
         <div className="Post__Bottom">
-            <div className="Post__Bottom__likes-container">
-                <i class="fa-solid fa-thumbs-up"></i>
-                <p>Like(s)</p>
+            <div className="Post__Bottom__likes-comments" onClick={() => {
+                setIsLiked(!isLiked );
+                setLikes(isLiked ? likes - 1 : likes + 1);
+            }}>
+                {isLiked ? <LikeBg /> : <LikeOutLine />}
+                <p>{likes} Like{likes > 1 ? 's' : ''}</p>
             </div>
-            <div className="Post__Bottom__comments-container">
+            <div className="Post__Bottom__likes-comments">
                 <i class="fa-solid fa-message"></i>
-                <p>Comment(s)</p>
+                <p>0 Comment(s)</p>
             </div>
         </div>
       </div>
