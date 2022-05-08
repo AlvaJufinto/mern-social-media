7b722dff79395a6046216df2d8f6b2b622dcbab3
unknown <yeahbruh199@gmail.com>
2022-05-08 10:49:42 +0700

    ADD: Explore page

---
+++ src/pages/Explore.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import {
+  Link
+} from "react-router-dom";
+
+import Navbar from "../components/Navbar";
+
+import './../styles/pages-css/explore.css';
+
+const Explore = () => {
+
+  return (
+    <>
+      <Navbar />
+      <div className="ExploreContainer section-global">
+        
+      </div>
+    </>
+  );
+}
+
+export default Explore;
