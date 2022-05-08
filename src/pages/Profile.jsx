7767fd5fcc95a3ec52c42b3fc893df3a4248c65e
unknown <yeahbruh199@gmail.com>
2022-05-08 21:21:40 +0700

    ADD: banner component on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import {
   Link
 } from "react-router-dom";
@@ -6,17 +6,49 @@ import {
 import Navbar from "../components/Navbar";
 import AddPost from "../components/AddPost";
 import Post from "../components/Post";
+
 import noAvatar from "./../assets/img/noAvatar.png";
+import noBanner from "./../assets/img/noBanner.jpg"
 
 import './../styles/pages-css/profile.css';
 
 const Profile = () => {
+  const [isSelf, setIsSelf] = useState(true);
 
   return (
     <>
       <Navbar />
       <div className="ProfileContainer section-global">
-      
+        <div className="Profile__left-side">
+          <div className="Profile__banner-container" style={{
+                backgroundImage: `url(${noBanner})`
+              }}>
+            <div className="Profile__banner-container__Content">
+              <img src={noAvatar} alt="profile img" className="Profile__Content__img" />
+              <div className="Profile__Content__bio">
+                <h1 className="Profile__bio__Username">Tom Shelby</h1>
+                <h2 className="Profile__bio__Fullname">Thomas Shelby</h2>
+                <p className="Profile__bio__Description">Slebew</p>
+                <div className="Profile__bio__Connections">
+                  <div className="Profile__Connection"><span>0</span> Followers</div>
+                  <div className="Profile__Connection"><span>0</span> Followings</div>
+                </div>
+              </div>
+              {
+                isSelf ?
+                  <button className="Profile__bio__Button button-global">Edit Profile</button>
+                :
+                  <button className="Profile__bio__Button button-global">Follow</button>
+              }
+            </div>
+          </div>
+          <div className="Profile__bottom-container">
+
+          </div>
+        </div>
+        <div className="Profile__right-side">
+          adsadssad 
+        </div>
       </div>
     </>
   );
